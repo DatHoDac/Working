fa73aca402bce43ead9f8692beb6c6cb491c79c9
DatHoDac <hodacanhdat@gmail.com>
2016-04-27 18:00:46 +0700

    capture scr - constructor - static/non static

--- Testing1/src/mytestclass/BasicActionCommands.java
+++ Testing1/src/mytestclass/BasicActionCommands.java
@@ -1,26 +1,26 @@
 package mytestclass;
 
-/*import org.openqa.selenium.By;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;;*/
+import org.openqa.selenium.firefox.FirefoxDriver;;
 
-/*public class BasicActionCommands {*/
-	/*public void main(String[] args){
-	
-	//1. Creating New Instance Of Firefox Driver
-	WebDriver driver = new FirefoxDriver();
-		
-	//2. Command To Open URL In Browser
-	driver.get("http://only-testing-blog.blogspot.com/2013/11/new-test.html");
-	
-	//3. Clicking on any element or button of webpage
-	//driver.findElement(By.id("submitButton")).click();
-	
-	//4. Store text of targeted element in variable
-	String dropdown = driver.findElement(By.tagName("select")).getText();
-	System.out.println(dropdown);
-	
-	
-	
-	}
-}*/
+//public class BasicActionCommands {
+////	public void main(String[] args){
+////	
+////	//1. Creating New Instance Of Firefox Driver
+////	WebDriver driver = new FirefoxDriver();
+////		
+////	//2. Command To Open URL In Browser
+////	driver.get("http://only-testing-blog.blogspot.com/2013/11/new-test.html");
+////	
+////	//3. Clicking on any element or button of webpage
+////	//driver.findElement(By.id("submitButton")).click();
+////	
+////	//4. Store text of targeted element in variable
+////	String dropdown = driver.findElement(By.tagName("select")).getText();
+////	System.out.println(dropdown);
+////	
+////	
+////	
+////	}
+//}
--- Testing1/src/mytestclass/mytestclass.java
+++ Testing1/src/mytestclass/mytestclass.java
@@ -1,30 +1,36 @@
 package mytestclass;
 
+//import java.io.File;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 
 
-import org.apache.james.mime4j.field.datetime.DateTime;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+//import org.apache.commons.io.FileUtils;
+//import org.apache.james.mime4j.field.datetime.DateTime;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.JavascriptExecutor;
+//import org.openqa.selenium.TakesScreenshot;
+//import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import com.gargoylesoftware.htmlunit.javascript.host.dom.Document;
-import com.google.common.base.Stopwatch;
+//import org.openqa.selenium.support.ui.ExpectedConditions;
+//import org.openqa.selenium.support.ui.Select;
+//import org.openqa.selenium.support.ui.WebDriverWait;
+//
+//import com.gargoylesoftware.htmlunit.javascript.host.dom.Document;
+//import com.google.common.base.Stopwatch;
 
 
 public class mytestclass {
 
 
-	 public static void main(String[] args) {
+	 public static void main(String[] args) throws InterruptedException, IOException {
 		  WebDriver driver = new FirefoxDriver();
 		  driver.get("http://only-testing-blog.blogspot.in");
+		  
 //		  String i = driver.getCurrentUrl();
 //		  System.out.println(i);
 //		  System.out.println("this is Dat");
@@ -37,21 +43,22 @@ public class mytestclass {
 //		  }else{
 //			  System.out.println("Alert isn't exist.");
 //		  }
-  
-//		  4. Store text of targeted element in variable
+//  
+//		  //4. Store text of targeted element in variable
 //		  String dropdown = driver.findElement(By.tagName("select")).getText();
 //		  System.out.println(dropdown);
-		  
-		  
-//		  5. Typing text in text box or text area.
+//		  
+//		  
+//		  //5. Typing text in text box or text area.
 //		  driver.findElement(By.name("fname")).sendKeys("My First Name");
-//		  6. Applying Implicit wait in webdriver
-//		  for (int i = 0; i < 15; i++) {
+//		  
+//		  //6. Applying Implicit wait in web driver
+//		  for (int e = 0; e < 15; e++) {
 //			  driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-//			  System.out.println("Second: " +  i);
+//			  System.out.println("Second: " +  e);
 //		}
-		  
-//		  7. Applying Explicit wait in webdriver with WebDriver canned conditions.  
+//		  
+//		  //7. Applying Explicit wait in web driver with WebDriver canned conditions.  
 //		   try {
 //			  System.out.println("lan 1: " + LocalDateTime.now()); 
 //			  WebDriverWait wait = new WebDriverWait(driver, 10);
@@ -61,36 +68,36 @@ public class mytestclass {
 //				System.out.println("String Time left: 7 seconds not exist in 10 secs");
 //				System.out.println("lan 2: " + LocalDateTime.now());	
 //			}
-					
-//		  10. Get domain name using java script executor
+//					
+//		  //10. Get domain name using java script executor
 //		  JavascriptExecutor javascript = (JavascriptExecutor) driver;
 //		  String CurrentURLUsingJS=(String)javascript.executeScript("return document.domain");
 //		  System.out.println("Domain is: " + CurrentURLUsingJS);	
-		  
-//		  11. Generate alert using webdriver's java script executor interface
-		  
-//		  JavascriptExecutor javascript = (JavascriptExecutor) driver;
-//		  javascript.executeScript("alert('Test Case Execution Is started Now..');");
-		  
-//		  12. Selecting or Deselecting value from drop down in selenium webdriver.
-//		  Select By Visible Text
+//		  
+//		  //11. Generate alert using webdriver's java script executor interface
+//		  
+//		  JavascriptExecutor javascript2 = (JavascriptExecutor) driver;
+//		  javascript2.executeScript("alert('Test Case Execution Is started Now..');");
+//		  
+//		  //12. Selecting or Deselecting value from drop down in selenium webdriver.
+//		  //Select By Visible Text
 //		  Select mydrpdwn = new Select(driver.findElement(By.id("Carlist")));
 //		  mydrpdwn.selectByVisibleText("Audi");
-		  
-//		  Select By Value
+//		  
+//		  //Select By Value
 //		  Select listbox = new Select(driver.findElement(By.xpath("//select[@name='FromLB']")));
 //		  listbox.selectByValue("Italy");
-		  
-//		  Select By Index
+//		  
+//		  //Select By Index
 //		  Select listbox2 = new Select(driver.findElement(By.xpath("//select[@name='FromLB']")));
 //		  listbox2.selectByIndex(0);
-		  
-//		  isMultiple()
-//		  Select listbox = new Select(driver.findElement(By.xpath("//select[@name='FromLB']")));
-//		  boolean value = listbox.isMultiple();
+//		  
+//		  //isMultiple()
+//		  Select listbox3 = new Select(driver.findElement(By.xpath("//select[@name='FromLB']")));
+//		  boolean value = listbox3.isMultiple();
 //		  System.out.println(value);
-		  
-//		  13. Navigate to URL or Back or Forward in Selenium Webdriver
+//		  
+//		  //13. Navigate to URL or Back or Forward in Selenium Webdriver
 //		  driver.navigate().to("http://only-testing-blog.blogspot.com/2014/01/textbox.html#sthash.9Q3Ew9bn.dpuf");
 //		  driver.navigate().back();
 //		  String a = driver.getTitle();
@@ -100,14 +107,19 @@ public class mytestclass {
 //		  String b = driver.getTitle();
 //		  String bb = driver.getCurrentUrl();
 //		  System.out.println(b + ";" + bb);
-		  
-//		  14. Verify Element Present in Selenium WebDriver
-		  Boolean isExist = driver.findElements(By.xpath("//input[@id='text2']")).size()!= 0;
-		  Boolean isExist2 = driver.findElements(By.xpath("//select[@name='FromLB']")).size()!= 0;
-		  
-		  System.out.println(isExist + " ; " + isExist2);
-//		  driver.close();
-		 }
-	 
+//		  
+//		  //14. Verify Element Present in Selenium WebDriver
+//		  Boolean isExist = driver.findElements(By.xpath("//input[@id='text2']")).size()!= 0;
+//		  Boolean isExist2 = driver.findElements(By.xpath("//select[@name='FromLB']")).size()!= 0;
+//		  System.out.println(isExist + " ; " + isExist2);
+//		  
+//		  //15. Capturing entire page screenshot in Selenium WebDriver
 
+		 BasicActions a = new BasicActions(driver);
+		 a.capturescr();
+		
+		 
+		 BasicActions.SayHello(driver);
+		 driver.close();
+		 }
 }
