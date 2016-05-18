cb939aa1e4d24fa5dae2c818b7181f78acd64e40
DatHoDac <hodacanhdat@gmail.com>
2016-05-18 18:17:59 +0700

    pageload document.stateready

--- TestNGOne/src/TestNGOnePack/GetPageTitle.java
+++ TestNGOne/src/TestNGOnePack/GetPageTitle.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -69,14 +70,40 @@ public class GetPageTitle {
 //		 
 //	 }
 	 
-	 //Generating Alert
+//	 //Generating Alert
+//	 @Test
+//	 public void test() throws InterruptedException {
+//		 JavascriptExecutor javascript = (JavascriptExecutor) driver;
+//		 javascript.executeScript("alert('The script is prepared to run...')");
+//		 Thread.sleep(5000);
+//		 driver.switchTo().alert().accept();
+//	 }
+	 
+	 
+	 public void HighlightMyElement(WebElement element){
+		 
+		 for(int i=0; i<10; i++)
+		 {
+			 JavascriptExecutor javascript = (JavascriptExecutor) driver;
+			 javascript.executeScript("arguments[0].setAttribute('style', arguments[1]);", element, "color: orange; border: 4px solid orange;");
+			 javascript.executeScript("arguments[0].setAttribute('style', arguments[1]);", element, "color: pink; border: 4px solid pink;");
+			 javascript.executeScript("arguments[0].setAttribute('style', arguments[1]);", element, "color: yellow; border: 4px solid yellow;");
+			 javascript.executeScript("arguments[0].setAttribute('style', arguments[1]);", element, ""); 
+		 }
+	 }
+	 
 	 
 	 @Test
-	 public void test() throws InterruptedException {
-		 JavascriptExecutor javascript = (JavascriptExecutor) driver;
-		 javascript.executeScript("alert('The script is prepared to run...')");
-		 Thread.sleep(5000);
-		 driver.switchTo().alert().accept();
+	 public void Test()
+	 {
+		 HighlightMyElement(driver.findElement(By.xpath("//input[@name='fname']")));
+		 driver.findElement(By.xpath("//input[@name='fname']")).sendKeys("My name");
+		 HighlightMyElement(driver.findElement(By.xpath("//button[@onclick='myFunction()']")));
+		 WebDriverWait wait = new WebDriverWait(driver, 15);
+		 wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//input[@id='submitButton']"))));
+		 HighlightMyElement(driver.findElement(By.xpath("//input[@id='submitButton']")));
+		 driver.findElement(By.xpath("//input[@id='submitButton']")).click();
+		 
 	 }
-	
+	 
 }
