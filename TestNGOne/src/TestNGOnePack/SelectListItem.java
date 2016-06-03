2be7c0d83ace0f62ffbec4086afc6724baba0396
DatHoDac <hodacanhdat@gmail.com>
2016-06-03 19:33:39 +0700

    abc

--- TestNGOne/src/TestNGOnePack/SelectListItem.java
+++ TestNGOne/src/TestNGOnePack/SelectListItem.java
@@ -3,6 +3,7 @@ package TestNGOnePack;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -24,33 +25,61 @@ public class SelectListItem {
 		 driver.get("http://only-testing-blog.blogspot.in/2014/01/textbox.html");
 	}
 	
-	@AfterTest
-	public void aftertest()
-	{
-		driver.quit();
-	}
+//	@AfterTest
+//	public void aftertest()
+//	{
+//		driver.quit();
+//	}
+	
+	
+//	//SELECT LIST ITEM 
+	
+//	@Test
+//	public void SelectTest() throws InterruptedException 
+//	{
+//		driver.findElement(By.xpath("//input[@id='text1']")).sendKeys("My name name name");
+//		
+//		Select listbox = new Select(driver.findElement(By.xpath("//select[@name='FromLB']")));
+//		listbox.selectByValue("Japan");
+//		listbox.selectByValue("Mexico");
+//		listbox.selectByValue("India");
+//		
+//		
+//		driver.findElement(By.xpath("//input[@value='->']")).click();
+//		
+//		WebDriverWait wait = new WebDriverWait(driver, 15);
+////		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//input[@id='text2']"))));
+//		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("text2"))));
+//		
+//		listbox.selectByIndex(0);
+//		listbox.selectByIndex(3);
+//		driver.findElement(By.xpath("//input[@value='->']")).click();
+//		
+//		Thread.sleep(5000);
+//	}
+	
 	
 	@Test
-	public void SelectTest() throws InterruptedException 
+	public void navigatefunc()
 	{
-		driver.findElement(By.xpath("//input[@id='text1']")).sendKeys("My name name name");
+		String title1= driver.getTitle();
+		System.out.println("load page at 1st time, title: " + title1);
 		
-		Select listbox = new Select(driver.findElement(By.xpath("//select[@name='FromLB']")));
-		listbox.selectByValue("Japan");
-		listbox.selectByValue("Mexico");
-		listbox.selectByValue("India");
+		driver.navigate().to("http://only-testing-blog.blogspot.in/2013/09/test.html");
+		String title2= driver.getTitle();
+		System.out.println("load page at 2nd time, title: " + title2);
 		
+		driver.navigate().back();
+		String title3= driver.getTitle();
+		System.out.println("load 1st page, title: " + title3);
 		
-		driver.findElement(By.xpath("//input[@value='->']")).click();
-		
-		WebDriverWait wait = new WebDriverWait(driver, 15);
-//		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//input[@id='text2']"))));
-		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("text2"))));
-		
-		listbox.selectByIndex(0);
-		listbox.selectByIndex(3);
-		driver.findElement(By.xpath("//input[@value='->']")).click();
-		
-		Thread.sleep(5000);
+		driver.navigate().forward();
+		String title4= driver.getTitle();
+		System.out.println("load 2nd page, title: " + title4);
 	}
+	
+	
+	
+	
+	
 }
